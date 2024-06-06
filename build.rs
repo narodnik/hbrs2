74f0bb698d30e8e6dc9466daf403fff7512d2db0
rsx <rsx@rsx>
2024-06-06 14:32:07 +0200

    freetype support for harfbuzz in build.rs

--- build.rs
+++ build.rs
@@ -10,18 +10,16 @@ fn main() {
     let target = env::var("TARGET").unwrap();
 
     println!("cargo:rerun-if-env-changed=HARFBUZZ_SYS_NO_PKG_CONFIG");
-    if target.contains("wasm32") || env::var_os("HARFBUZZ_SYS_NO_PKG_CONFIG").is_none() {
-        if pkg_config::probe_library("harfbuzz").is_ok() {
-            return;
-        }
-    }
 
     let mut cfg = cc::Build::new();
     cfg.cpp(true)
         .flag("-std=c++11")
         .warnings(false)
         .include("harfbuzz/src")
+        .include("freetype-sys/freetype2/include/")
         .file("harfbuzz/src/harfbuzz.cc");
+
+    cfg.define("HAVE_FREETYPE", "1");
 
     if !target.contains("windows") {
         cfg.define("HAVE_PTHREAD", "1");
@@ -36,6 +34,8 @@ fn main() {
     }
 
     cfg.compile("embedded_harfbuzz");
+
+    println!("cargo::rustc-link-lib=static=embedded_harfbuzz");
 }
 
 #[cfg(not(feature = "build-native-harfbuzz"))]
